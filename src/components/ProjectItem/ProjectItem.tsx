6f28c4de6228ab59a5d035e9de52695ebf0e16c7
Eloagbawe <eloagbawe@gmail.com>
2024-07-26 13:34:14 -0400

    add projectitem and projects component

---
+++ src/components/ProjectItem/ProjectItem.tsx
@@ -0,0 +1,46 @@
+import "./ProjectItem.scss";
+import { FC } from "react";
+
+interface ProjectProps {
+  project: {
+    name: string;
+    description: string;
+    tools: string;
+    link: string;
+    github: string;
+    backend?: string;
+  };
+}
+
+const ProjectItem: FC<ProjectProps> = ({ project }) => {
+  return (
+    <div className="project p-5 my-8 h-[15rem] rounded-lg flex flex-col gap-2 justify-between">
+      <h4 className="project__title">{project.name}</h4>
+      <p className="project__description">{project.description}</p>
+      <p className="project__tools">{project.tools}</p>
+
+      <div className="flex flex-wrap gap-2">
+        <a href={project.link} className="project__link w-fit">
+          Live
+        </a>
+
+        {project.backend ? (
+          <>
+            <a href={project.github} className="project__link w-fit">
+              Github : Frontend
+            </a>
+            <a href={project.backend} className="project__link w-fit">
+              Github : Backend
+            </a>
+          </>
+        ) : (
+          <a href={project.github} className="project__link w-fit">
+            Github
+          </a>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectItem;
